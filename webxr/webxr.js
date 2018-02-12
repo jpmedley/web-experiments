43357383c956132dadebca1527ee444ecb59c150
Joseph Medley <jmedley@google.com>
2018-02-12 15:44:00 -0800

    Start building exclusive/non-exclusive sessions.

--- webxr/webxr.js
+++ webxr/webxr.js
@@ -3,48 +3,45 @@
 import Logger from "../logger.js";
 const logger = new Logger("WebXR");
 
+const vrCanvas = document.querySelector("#vr");
+let xrDevice;
+
 function setUpXR() {
   // It appears that I need a head-mounted display to test this with.
   navigator.xr.addEventListener('devicechange', (e) => {
     logger.log(e.target);
-    getSession();
   }, (err) => {
     logger.error("Something went wrong.", err);
   });
-}
 
-function getSession() {
-  // Has the wrong type which causes supportsSession() to blow up.
-  const xrContext = window.XRPresentationContext
-  const sessionOptions = {
-    exclusive: true,
-    outputContext: xrContext
-  }
   navigator.xr.requestDevice()
   .then(device => {
-    device.supportsSession(sessionOptions)
-    .then(() => {
-      device.requestSession(sessionOptions)
-      .then(session => {
-        logger.log(session.exclusive)
-      });
-    });
-  });
-}
-
-function getDevice() {
-  // As per spec, but doesn't currently work because Chrome throws 'Uncaught
-  //   type error instead of rejecting the Promise with 'NotFoundError'.
-  navigator.xr.requestDevice()
-  .then(xrDevice => {
-    logger.log(xrDevice.external);
+    xrDevice = device
   })
   .catch(err => {
-    logger.error("Unable to find XR device.");
-  });
+    logger.error("No device was found. ", err);
+  })
 }
 
+function getNESession() {
+  const sessionOptions = {
+    exclusive: false,
+    outputContext: vrCanvas.getContext('xrpresent');
+  }
+  if (xrDevice) {
+    xrDevice.requestSession(sessionOptions)
+    .then(session => {
+      //Do something with the session.
+    })
+    .catch(err => {
+      logger.error("AR/VR session could not be created. ", err);
+    });
+  }
+}
+
+
+
 export {
-  getDevice,
+  getNESession,
   setUpXR
 }
