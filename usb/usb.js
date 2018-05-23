e956aec0513b5907537aff12716ed9e889c5b97b
Joseph Medley <jmedley@google.com>
2018-05-23 13:38:35 -0700

    Device enumeration experiments.

--- usb/usb.js
+++ usb/usb.js
@@ -1,5 +1,35 @@
 'use strict';
 
+async function requestDevice() {
+  const filters = [
+          {vendorId: 0x1209, productId: 0xa800},
+      ];
+  let device = await navigator.usb.requestDevice({filters: filters})
+                     .catch(e => {
+                       console.error("There is no device. " + e);
+                     });
+  if (device !== undefined) {
+    console.log("Product name: " + device.productName);
+  }
+}
+
+async function requestDevice_() {
+  // Spec example.
+  let device;
+  const filters = [
+          {vendorId: 0x1209, productId: 0xa800},
+        ];
+  try {
+    device = await navigator.usb.requestDevice({filters: filters});
+  } catch (e) {
+    console.error("There is no device." + e);
+  }
+
+  if (device !== undefined) {
+    console.log("Product name: " + device.productName);
+  }
+}
+
 function enumerateDevices() {
   navigator.usb.getDevices()
   .then(devices => {
@@ -14,7 +44,13 @@ function deviceAdded(e) {
   console.log(e.device);
 }
 
+function deviceRemoved(e) {
+  console.log(e);
+}
+
 export {
   deviceAdded,
-  enumerateDevices
+  deviceRemoved,
+  enumerateDevices,
+  requestDevice
 };
