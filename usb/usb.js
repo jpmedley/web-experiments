e80bbff64c888b2a769f7dbee94011bc7d72da18
Joseph Medley <jmedley@google.com>
2018-05-24 13:51:11 -0700

    More device enumeration experiments.

--- usb/usb.js
+++ usb/usb.js
@@ -1,33 +1,11 @@
 'use strict';
 
-async function requestDevice() {
-  const filters = [
-          {vendorId: 0x1209, productId: 0xa800},
-      ];
-  let device = await navigator.usb.requestDevice({filters: filters})
-                     .catch(e => {
-                       console.error("There is no device. " + e);
-                     });
-  if (device !== undefined) {
-    console.log("Product name: " + device.productName);
-  }
+function deviceAdded(e) {
+  console.log(e.device);
 }
 
-async function requestDevice_() {
-  // Spec example.
-  let device;
-  const filters = [
-          {vendorId: 0x1209, productId: 0xa800},
-        ];
-  try {
-    device = await navigator.usb.requestDevice({filters: filters});
-  } catch (e) {
-    console.error("There is no device." + e);
-  }
-
-  if (device !== undefined) {
-    console.log("Product name: " + device.productName);
-  }
+function deviceRemoved(e) {
+  console.log(e);
 }
 
 function enumerateDevices() {
@@ -35,17 +13,22 @@ function enumerateDevices() {
   .then(devices => {
     console.log("Total devices: " + devices.length);
     devices.forEach(device => {
-      console.log("Product name: " + device.productName);
+      console.log("Product name: " + device.productName + ", serial number " + device.serialNumber);
     });
   });
 }
 
-function deviceAdded(e) {
-  console.log(e.device);
-}
-
-function deviceRemoved(e) {
-  console.log(e);
+async function requestDevice() {
+  const filters = [
+          {vendorId: 0x1209, productId: 0xa800},
+        ];
+  await navigator.usb.requestDevice({filters: filters})
+        .then(device => {
+          console.log("Product name: " + device.productName);
+        })
+        .catch(e => {
+          console.log("There is not device. " + e);
+        });
 }
 
 export {
